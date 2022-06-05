053b2bf95139edd17b01a945d57182eef6d4280a
afelicetti77 <afelicetti.iq77@gmail.com>
2022-06-05 11:27:35 -0400

    estoy guardando un cambio en la segunda_rama

--- index3.js
+++ index3.js
@@ -3,3 +3,4 @@ const os = require('os');
 console.log(os.release());
 console.log('probando cambios en git1');
 console.log('probando cambios en git2');
+console.log('probando cambios en git3, esto deberia guardarse en la segunda rama');
