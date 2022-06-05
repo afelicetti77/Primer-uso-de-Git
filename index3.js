7a4443fcd6d328f9197fee895a42a6d81c172a8c
afelicetti77 <afelicetti.iq77@gmail.com>
2022-06-05 16:36:37 -0400

    subiendo segunda rama para probar el merge

--- index3.js
+++ index3.js
@@ -5,3 +5,4 @@ console.log('probando cambios en git1');
 console.log('probando cambios en git2');
 console.log('probando cambios en git3, esto deberia guardarse en la segunda rama');
 console.log('despeus del merge, probando cambios en git3, esto deberia guardarse en la segunda rama');
+console.log('vamos a hacer un merge y esto deberia aparecer en main');
