1167f2bd9372360c9d1e685010e0fdacbfae2d54
afelicetti77 <afelicetti.iq77@gmail.com>
2022-06-05 12:28:19 -0400

    subiendo a egunda rama despues del primer merge

--- index3.js
+++ index3.js
@@ -4,3 +4,4 @@ console.log(os.release());
 console.log('probando cambios en git1');
 console.log('probando cambios en git2');
 console.log('probando cambios en git3, esto deberia guardarse en la segunda rama');
+console.log('despeus del merge, probando cambios en git3, esto deberia guardarse en la segunda rama');
